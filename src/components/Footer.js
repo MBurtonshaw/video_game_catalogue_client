ef0e5200f3e415408eb28ee586f1fb244c585321
Matt <mburtonshaw@gmail.com>
2024-09-18 15:06:50 -0400

    fixed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
 
     return (
         <div id='Footer' className='mt-5 pt-2'>
-            <a class='nonchalant' target='_blank' href='https://verdant-cranachan-f205b0.netlify.app/'>
+            <a class='nonchalant' target='_blank' href='https://matthew-burtonshaw.netlify.app/'>
                 <p className='mt-5 fs-5'>by Matthew Burtonshaw</p>
             </a>
         </div>
